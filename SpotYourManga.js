01d40190ef98707b8ca2cf287e3b0366a8ec243c
Stefano Baghino <k0but4@gmail.com>
2014-01-05 00:54:03 +0100

    Added local storage of favorites

--- SpotYourManga.js
+++ SpotYourManga.js
@@ -1,27 +1,51 @@
 // ==UserScript==
 // @name       SpotYourManga
 // @namespace  http://gabrielelana.it
-// @version    0.0.1
+// @version    0.0.2
 // @description  Some manga have an hard name to remember, this will let you spot your mangas in the mangareader.net home page
 // @match      http://www.mangareader.net/
 // @copyright  2013, Gabriele Lana
 // @require    http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js
 // ==/UserScript==
 
-// TODO: make the search more reliable (fuzzy match)
-// TODO: save the manga list in the local storage
-// TODO: add to every manga not tracked a button to add it to the list
+(function ($) {
+    
+    var favManga, $favMangaMarker;
+    
+    function editFavorite() {
+        var target = $(this)
+        var clickedTitle = target.next().text()
+        var titleIndex = favManga.indexOf(clickedTitle)
+        console.log(titleIndex)
+        if (titleIndex >= 0) {
+            favManga.splice(titleIndex, 1)
+            target.css('color', 'black')
+        } else {
+            favManga.push(clickedTitle)
+            target.css('color', 'orange')
+        }
+        localStorage.favManga = favManga
+    }
+    
+    function spawnMarker() {
+        return $('<span />')
+    		.css({
+                'margin-right': '10px',
+                'font-size': '1.2em',
+                'position': 'relative',
+                'cursor': 'pointer'
+            })
+    		.html('&#x2605;')
+            .on('click', editFavorite)
+    }
+    
+    favManga = localStorage.favManga && localStorage.favManga.split(',') || []
+    
+    $('.chapter')
+    	.each(function() {
+            var isFav = favManga.indexOf($(this).text()) >= 0
+            $(this).parent().prepend(spawnMarker().css('color', isFav ? 'orange' : 'black'))
+        })
 
-var $spottedMangaMarker = $('<span style="margin-left: 10px; color: black; font-size: 1.2em; position: absolute;">&#x2605;</span>')
-var mangaToKeepTrackOf = [
-    'Naruto', 'Bleach', 'Dragons Rioting', 'Beelzebub', 'The Breaker: New Waves', 'Fairy Tail',
-    'Historys Strongest Disciple Kenichi', 'Girls of the Wild\'s', 'Diamond no Ace', 'One Piece',
-    'Freezing', 'Sekirei'
-]
+}(jQuery));
 
-$('.chapter strong')
-    .filter(function() {
-        return mangaToKeepTrackOf.indexOf($(this).text()) >= 0
-    })
-    .parent()
-    .append($spottedMangaMarker)
