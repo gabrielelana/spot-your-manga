703f1b39c1648a19789485dc4c62c830ada99b68
gabriele.lana <gabriele.lana@cleancode.it>
2014-01-05 16:46:32 +0100

    metadata: authors and more appropriate namespace

--- SpotYourManga.js
+++ SpotYourManga.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name     SpotYourManga
-// @namespace  http://gabrielelana.it
-// @version  0.0.2
+// @name         SpotYourManga
+// @namespace    https://github.com/gabrielelana/spot-your-manga
+// @author       https://github.com/gabrielelana
+// @author       https://github.com/stefanobaghino
+// @version      0.0.2
 // @description  Some manga have an hard name to remember, this will let you spot your mangas in the mangareader.net home page
-// @match    http://www.mangareader.net/
-// @copyright  2013, Gabriele Lana
-// @require  http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js
+// @match        http://www.mangareader.net/
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js
 // ==/UserScript==
 
 (function ($) {
