525bed9a8a7100a498b3d31312734078c8bf333f
gabriele.lana <gabriele.lana@cleancode.it>
2014-01-05 17:14:04 +0100

    keep the indentation that Greasemonkey/Tampermonkey editor wants :-)

--- SpotYourManga.js
+++ SpotYourManga.js
@@ -9,37 +9,37 @@
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js
 // ==/UserScript==
 
-(function ($) {
-  
-  var starred = localStorage.starred && JSON.parse(localStorage.starred) || []
-
-  var emptyStarSymbol = '&#x2606', fullStarSymbol = '&#x2605'
-  var $marker = $('<span />')
+(function($) {
+    
+    var starred = localStorage.starred && JSON.parse(localStorage.starred) || []
+    
+    var emptyStarSymbol = '&#x2606', fullStarSymbol = '&#x2605'
+    var $marker = $('<span />')
     .css({
-      'margin-right': '10px',
-      'font-size': '1.2em',
-      'position': 'relative',
-      'cursor': 'pointer'
+        'margin-right': '10px',
+        'font-size': '1.2em',
+        'position': 'relative',
+        'cursor': 'pointer'
     })
     .on('click', toggleStar)
-
-  $('.chapter').each(function() {
-    var isStarred = starred.indexOf($(this).text()) >= 0
-    $(this).parent().prepend($marker.clone().html(isStarred ? fullStarSymbol : emptyStarSymbol))
-  })
-  
-  function toggleStar() {
-    var target = $(this)
-    var clickedTitle = target.next().text()
-    var titleIndex = starred.indexOf(clickedTitle)
-    if (titleIndex >= 0) {
-      starred.splice(titleIndex, 1)
-      target.html(emptyStarSymbol)
-    } else {
-      starred.push(clickedTitle)
-      target.html(fullStarSymbol)
+    
+    $('.chapter').each(function() {
+        var isStarred = starred.indexOf($(this).text()) >= 0
+        $(this).parent().prepend($marker.clone().html(isStarred ? fullStarSymbol : emptyStarSymbol))
+    })
+    
+    function toggleStar() {
+        var target = $(this)
+        var clickedTitle = target.next().text()
+        var titleIndex = starred.indexOf(clickedTitle)
+        if (titleIndex >= 0) {
+            starred.splice(titleIndex, 1)
+            target.html(emptyStarSymbol)
+        } else {
+            starred.push(clickedTitle)
+            target.html(fullStarSymbol)
+        }
+        localStorage.starred = JSON.stringify(starred)
     }
-    localStorage.starred = JSON.stringify(starred)
-  }
     
 }(jQuery));
